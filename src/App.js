7c824d065453633105ea8f422afebba8ecfd6c0e
gayathriv93 <gayathri.babu23@gmail.com>
2021-11-17 17:26:35 +0530

    Initial screen done

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { Switch,BrowserRouter,Route, Redirect} from 'react-router-dom';
 import Navbar from './Navbar-component/navbar';
+import Initial from './Initial-Component/Initialcomponent';
 
 function App() {
   return (
    <div>
-      <Navbar />
+     <Initial></Initial>
+      {/* <Navbar /> */}
      
    </div>
   );
---
+++ src/Initial-Component/Initialcomponent.js
@@ -0,0 +1,36 @@
+import { Component } from "react";
+import './Initialcomponent.css';
+import topimg from '../images/young-smiling-stu.png';
+
+
+
+class Initial extends Component {
+    render() {
+        return (
+            <div className="container-fluid position-relative ">
+                <div className="row bg-color ">
+                    <div className="col-4">
+                        <section className="positioning">
+                            <div className="row d-flex align-items-center h-100 w-100" >
+                                <span className="dummy-button " ></span>
+                                <h1 className="big-head fw-bold text-white mt-4">Study in Germany</h1>
+                                <span className="text-light small-head mt-3">One of the leading study destinations</span>
+                                <div>
+                                    <button className="btn contact-btn mt-5">
+                                        Contact Us
+                                    </button>
+                                </div>
+                            </div>
+                        </section>
+                    </div>
+                    <section className="col-8 img-bg position-relative">
+                        <img src={topimg} alt="" className="img-position"></img>
+                    </section>
+                </div>
+            </div>
+
+        )
+    }
+}
+
+export default Initial;
